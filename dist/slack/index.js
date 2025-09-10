e8d9a9c8f5840425d1a077bf29a4e02aa0f0dbe5
Kuba <kuba.krol.ldz@gmail.com>
2025-09-10 20:14:06 +0200

    remove dist from repo

--- dist/slack/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommandManager = CommandManager;
-const list_1 = require("./commands/list");
-const login_1 = require("./commands/login");
-const commands = [list_1.List, login_1.Login];
-function CommandManager() {
-    commands.forEach(cmd => cmd());
-}
