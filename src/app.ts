17b2512f506b07b42b9322a78d61f891c54c37ec
Kuba <kuba.krol.ldz@gmail.com>
2025-09-16 15:26:31 +0200

    Preparing the backend for render

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,12 @@ receiver.app.use('/api', authRouter);
 
 CommandManager();
 
-receiver.app.use(express.static(path.join(__dirname, 'public')));
+receiver.app.use(express.static(path.join(__dirname, '..', 'public')));
 
-receiver.app.set('views', path.join(__dirname, 'views'));
+
+receiver.app.set('views', path.join(__dirname, '..', 'src', 'views'));
 receiver.app.set('view engine', 'ejs');
+
 
 (async () => {
   await app.start(ENV.PORT);
