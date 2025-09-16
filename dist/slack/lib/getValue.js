a5c15a515fe31de2ff57586b139db7e39fb800ca
Kuba <kuba.krol.ldz@gmail.com>
2025-09-16 18:11:36 +0200

    remove dist

--- dist/slack/lib/getValue.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GetValue = void 0;
-const GetValue = (values, actionId) => {
-    return Object.values(values)
-        .map((block) => block[actionId]?.selected_option?.value || block[actionId]?.selected_date)
-        .find(Boolean);
-};
-exports.GetValue = GetValue;
