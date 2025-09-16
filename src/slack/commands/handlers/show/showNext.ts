2212bbcc4e74b3ff22f0b2100b3df0c3f97bd612
Kuba Król <kuba.krol.ldz@gmail.com>
2025-09-16 12:32:04 +0200

    Validation fixes in ShowNext, ShowWeek, ShowToday functions

--- src/slack/commands/handlers/show/showNext.ts
+++ src/slack/commands/handlers/show/showNext.ts
@@ -1,89 +1,97 @@
 import { RespondFn } from '@slack/bolt';
-import { calendar } from '../../../../config/google';
+import { calendar, setCredentialsForUser } from '../../../../config/google';
 import { PARKING_SLOTS } from '../../../constants/parkingSlots';
 import convertCalendarEvent from '../../../lib/convertEvent';
 import { GoogleEvent } from '../../../../models/googleEvent';
 import { ENV } from '../../../../utils/env';
-import { CheckAuth } from '../../../lib/checkAuth';
 import { generateDates } from '../../../lib/generateDates';
 import { GetWeek } from '../../../lib/getWeek';
+import { getToken } from '../../../../utils/tokenStorage';
 
 export const showNext = async (user_id: string, respond: RespondFn) => {
-  CheckAuth(user_id, respond);
+  if (!getToken(user_id)) {
+    await respond({
+      response_type: 'ephemeral',
+      text: 'You must be logged in via Google Auth. Use the `/parking login` command.',
+    });
+    return;
+  } else {
+    setCredentialsForUser(user_id);
 
-  const nextWeekDate = new Date();
-  nextWeekDate.setDate(nextWeekDate.getDate() + 7);
+    const nextWeekDate = new Date();
+    nextWeekDate.setDate(nextWeekDate.getDate() + 7);
 
-  const { start, end } = GetWeek(nextWeekDate);
+    const { start, end } = GetWeek(nextWeekDate);
 
-  const { data } = await calendar.events.list({
-    calendarId: ENV.GOOGLE_CALENDAR_ID,
-    timeMin: start.toISOString(),
-    timeMax: end.toISOString(),
-    singleEvents: true,
-    orderBy: 'startTime',
-  });
+    const { data } = await calendar.events.list({
+      calendarId: ENV.GOOGLE_CALENDAR_ID,
+      timeMin: start.toISOString(),
+      timeMax: end.toISOString(),
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
 
-  const dates = generateDates(start, end);
+    const dates = generateDates(start, end);
 
-  const takenDays = (slot: string): string[] => {
-    return ((data.items as GoogleEvent[]) || [])
-      .map(convertCalendarEvent)
-      .filter((a) => a.summary === slot)
-      .map((a) => a.start!)
-      .filter(Boolean);
-  };
+    const takenDays = (slot: string): string[] => {
+      return ((data.items as GoogleEvent[]) || [])
+        .map(convertCalendarEvent)
+        .filter((a) => a.summary === slot)
+        .map((a) => a.start!)
+        .filter(Boolean);
+    };
 
-  const statusLines = (parkings_days: string[]): string => {
-    return dates
-      .map((date) => {
-        return parkings_days.includes(date) ? '[❌]' : '[✅]';
-      })
-      .join(' ');
-  };
+    const statusLines = (parkings_days: string[]): string => {
+      return dates
+        .map((date) => {
+          return parkings_days.includes(date) ? '[❌]' : '[✅]';
+        })
+        .join(' ');
+    };
 
-  const statusColumn =
-    '*Status:*\n' +
-    PARKING_SLOTS.map((slot) => {
-      const parkings_days = takenDays(slot);
+    const statusColumn =
+      '*Status:*\n' +
+      PARKING_SLOTS.map((slot) => {
+        const parkings_days = takenDays(slot);
 
-      return statusLines(parkings_days);
-    }).join('\n');
+        return statusLines(parkings_days);
+      }).join('\n');
 
-  const parkingColumn = '*Parking:*\n' + PARKING_SLOTS.join('\n');
+    const parkingColumn = '*Parking:*\n' + PARKING_SLOTS.join('\n');
 
-  await respond({
-    response_type: 'in_channel',
-    attachments: [
-      {
-        color: '#E42930',
-        blocks: [
-          {
-            type: 'rich_text',
-            elements: [
-              {
-                type: 'rich_text_section',
-                elements: [
-                  { type: 'text', text: `List of available and taken parking slot this ` },
-                  {
-                    type: 'text',
-                    text: `${start.toLocaleDateString('pl-PL')} - ${end.toLocaleDateString('pl-PL')}`,
-                    style: { bold: true },
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            type: 'section',
-            fields: [
-              { type: 'mrkdwn', text: parkingColumn },
-              { type: 'mrkdwn', text: statusColumn },
-            ],
-          },
-          { type: 'divider' },
-        ],
-      },
-    ],
-  });
+    await respond({
+      response_type: 'in_channel',
+      attachments: [
+        {
+          color: '#E42930',
+          blocks: [
+            {
+              type: 'rich_text',
+              elements: [
+                {
+                  type: 'rich_text_section',
+                  elements: [
+                    { type: 'text', text: `List of available and taken parking slot this ` },
+                    {
+                      type: 'text',
+                      text: `${start.toLocaleDateString('pl-PL')} - ${end.toLocaleDateString('pl-PL')}`,
+                      style: { bold: true },
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'section',
+              fields: [
+                { type: 'mrkdwn', text: parkingColumn },
+                { type: 'mrkdwn', text: statusColumn },
+              ],
+            },
+            { type: 'divider' },
+          ],
+        },
+      ],
+    });
+  }
 };
--- src/slack/commands/handlers/show/showWeek.ts
+++ src/slack/commands/handlers/show/showWeek.ts
@@ -1,88 +1,96 @@
 import { RespondFn } from '@slack/bolt';
-import { calendar } from '../../../../config/google';
+import { calendar, setCredentialsForUser } from '../../../../config/google';
 import { PARKING_SLOTS } from '../../../constants/parkingSlots';
 import convertCalendarEvent from '../../../lib/convertEvent';
 import { GoogleEvent } from '../../../../models/googleEvent';
 import { ENV } from '../../../../utils/env';
-import { CheckAuth } from '../../../lib/checkAuth';
 import { GetWeek } from '../../../lib/getWeek';
 import { generateDates } from '../../../lib/generateDates';
+import { getToken } from '../../../../utils/tokenStorage';
 
 export const showWeek = async (user_id: string, respond: RespondFn) => {
-  CheckAuth(user_id, respond);
+  if (!getToken(user_id)) {
+    await respond({
+      response_type: 'ephemeral',
+      text: 'You must be logged in via Google Auth. Use the `/parking login` command.',
+    });
+    return;
+  } else {
+    setCredentialsForUser(user_id);
 
-  const { start, end } = GetWeek(new Date());
+    const { start, end } = GetWeek(new Date());
 
-  const { data } = await calendar.events.list({
-    calendarId: ENV.GOOGLE_CALENDAR_ID,
-    timeMin: start.toISOString(),
-    timeMax: end.toISOString(),
-    singleEvents: true,
-    orderBy: 'startTime',
-  });
+    const { data } = await calendar.events.list({
+      calendarId: ENV.GOOGLE_CALENDAR_ID,
+      timeMin: start.toISOString(),
+      timeMax: end.toISOString(),
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
 
-  const today = new Date().toISOString().split('T')[0];
-  const dates = generateDates(start, end);
+    const today = new Date().toISOString().split('T')[0];
+    const dates = generateDates(start, end);
 
-  const takenDays = (slot: string): string[] => {
-    return ((data.items as GoogleEvent[]) || [])
-      .map(convertCalendarEvent)
-      .filter((a) => a.summary === slot)
-      .map((a) => a.start!)
-      .filter(Boolean);
-  };
+    const takenDays = (slot: string): string[] => {
+      return ((data.items as GoogleEvent[]) || [])
+        .map(convertCalendarEvent)
+        .filter((a) => a.summary === slot)
+        .map((a) => a.start!)
+        .filter(Boolean);
+    };
 
-  const statusLines = (parkings_days: string[]): string => {
-    return dates
-      .map((date) => {
-        if (date < today) return '[❌]';
-        return parkings_days.includes(date) ? '[❌]' : '[✅]';
-      })
-      .join(' ');
-  };
+    const statusLines = (parkings_days: string[]): string => {
+      return dates
+        .map((date) => {
+          if (date < today) return '[❌]';
+          return parkings_days.includes(date) ? '[❌]' : '[✅]';
+        })
+        .join(' ');
+    };
 
-  const statusColumn =
-    '*Status:*\n' +
-    PARKING_SLOTS.map((slot) => {
-      const parkings_days = takenDays(slot);
+    const statusColumn =
+      '*Status:*\n' +
+      PARKING_SLOTS.map((slot) => {
+        const parkings_days = takenDays(slot);
 
-      return statusLines(parkings_days);
-    }).join('\n');
+        return statusLines(parkings_days);
+      }).join('\n');
 
-  const parkingColumn = '*Parking:*\n' + PARKING_SLOTS.join('\n');
+    const parkingColumn = '*Parking:*\n' + PARKING_SLOTS.join('\n');
 
-  await respond({
-    response_type: 'in_channel',
-    attachments: [
-      {
-        color: '#E42930',
-        blocks: [
-          {
-            type: 'rich_text',
-            elements: [
-              {
-                type: 'rich_text_section',
-                elements: [
-                  { type: 'text', text: `List of available and taken parking slot this ` },
-                  {
-                    type: 'text',
-                    text: `${start.toLocaleDateString('pl-PL')} - ${end.toLocaleDateString('pl-PL')}`,
-                    style: { bold: true },
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            type: 'section',
-            fields: [
-              { type: 'mrkdwn', text: parkingColumn },
-              { type: 'mrkdwn', text: statusColumn },
-            ],
-          },
-          { type: 'divider' },
-        ],
-      },
-    ],
-  });
+    await respond({
+      response_type: 'in_channel',
+      attachments: [
+        {
+          color: '#E42930',
+          blocks: [
+            {
+              type: 'rich_text',
+              elements: [
+                {
+                  type: 'rich_text_section',
+                  elements: [
+                    { type: 'text', text: `List of available and taken parking slot this ` },
+                    {
+                      type: 'text',
+                      text: `${start.toLocaleDateString('pl-PL')} - ${end.toLocaleDateString('pl-PL')}`,
+                      style: { bold: true },
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'section',
+              fields: [
+                { type: 'mrkdwn', text: parkingColumn },
+                { type: 'mrkdwn', text: statusColumn },
+              ],
+            },
+            { type: 'divider' },
+          ],
+        },
+      ],
+    });
+  }
 };
