53f05350bd327bac5536190a4f06e54faa332cc4
Kuba <kuba.krol.ldz@gmail.com>
2025-09-16 17:04:45 +0200

    configure app and vercel

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import { app, receiver } from './config/slack';
+import { receiver } from './config/slack';
 import { CommandManager } from './slack';
 import authRouter from './routes';
 import { ENV } from './utils/env';
 import express from 'express';
 import path from 'path';
+import serverless from 'serverless-http';
 
 receiver.app.use('/api', authRouter);
 
@@ -15,11 +16,17 @@ receiver.app.use(express.static(path.join(__dirname, 'public')));
 receiver.app.set('views', path.join(__dirname, 'views'));
 receiver.app.set('view engine', 'ejs');
 
-// receiver.app.get('/views', async (req, res) => {
-//   res.render('auth-success.ejs', { url: req.protocol + '://' + req.headers.host });
-// });
+receiver.app.get('/', async (req, res) => {
+  res.render('auth-success.ejs', { url: req.protocol + '://' + req.headers.host });
+});
 
-(async () => {
-  await app.start(ENV.PORT);
-  console.log('⚡ FDTParkingBot bot running with ExpressReceiver!');
-})();
+// (async () => {
+//   await app.start(ENV.PORT);
+//   console.log('⚡ FDTParkingBot bot running with ExpressReceiver!');
+// })();
+
+receiver.app.listen(ENV.PORT, () => {
+  console.log(`Server running at http://localhost:${ENV.PORT}`);
+})
+
+export const handler = serverless(receiver.app);
