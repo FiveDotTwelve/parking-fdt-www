3035b6677462fa43f13e1c071e930c5ea0f3f81e
Kuba <kuba.krol.ldz@gmail.com>
2025-09-16 18:05:00 +0200

    testing #3

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,19 @@ import { app, receiver } from './config/slack';
 import { CommandManager } from './slack';
 import authRouter from './routes';
 import { ENV } from './utils/env';
-import express from 'express';
-import path from 'path';
+// import express from 'express';
+// import path from 'path';
 import serverless from 'serverless-http';
 
 receiver.app.use('/api', authRouter);
 
 CommandManager();
 
-receiver.app.use(express.static(path.join(__dirname, 'public')));
+// receiver.app.use(express.static(path.join(__dirname, 'public')));
 
 
-receiver.app.set('views', path.join(__dirname, 'views'));
-receiver.app.set('view engine', 'ejs');
+// receiver.app.set('views', path.join(__dirname, 'views'));
+// receiver.app.set('view engine', 'ejs');
 
 receiver.app.get('/', (req, res) => {
   res.json({ ok: true, message: 'API działa!' });
