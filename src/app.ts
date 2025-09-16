73bc1ffaec5c07b09140306b8d85c291f4746a16
Kuba <kuba.krol.ldz@gmail.com>
2025-09-16 18:10:21 +0200

    Revert "small corrections"
    This reverts commit c44d02c839f209e6bc07f107840f857e19c360cf.

--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,20 @@ import { app, receiver } from './config/slack';
 import { CommandManager } from './slack';
 import authRouter from './routes';
 import { ENV } from './utils/env';
-// import express from 'express';
-// import path from 'path';
-import serverless from 'serverless-http';
+import express from 'express';
+import path from 'path';
 
 receiver.app.use('/api', authRouter);
 
 CommandManager();
 
-// receiver.app.use(express.static(path.join(__dirname, 'public')));
+receiver.app.use(express.static(path.join(__dirname, '..', 'public')));
 
+receiver.app.set('views', path.join(__dirname, '..', 'src', 'views'));
+receiver.app.set('view engine', 'ejs');
 
-// receiver.app.set('views', path.join(__dirname, 'views'));
-// receiver.app.set('view engine', 'ejs');
-
-receiver.app.get('/', (req, res) => {
-  res.json({ ok: true, message: 'API działa!' });
-
-});
-
-if (ENV.NODE_ENV !== 'production') {
-  (async () => {
-    await app.start(ENV.PORT);
-    console.log('⚡ FDTParkingBot running locally!');
-    console.log(`Server running at http://localhost:${ENV.PORT}`);
-  })();
-}
-
-export const handler = serverless(receiver.app);
+(async () => {
+  await app.start(ENV.PORT);
+  console.log('⚡ FDTParkingBot running locally!');
+  console.log(`Server running at http://localhost:${ENV.PORT}`);
+})();
