8e906bbe9b5ed2da1749628618fd09013496b71b
Kuba Król <kuba.krol.ldz@gmail.com>
2025-09-17 21:56:18 +0200

    deleted start

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import { app, receiver } from './config/slack';
+import { receiver } from './config/slack';
 import { CommandManager } from './slack';
 import authRouter from './routes';
-import { ENV } from './utils/env';
 import express from 'express';
 import path from 'path';
 
@@ -14,8 +13,5 @@ receiver.app.use(express.static(path.join(__dirname, 'public')));
 receiver.app.set('views', path.join(__dirname, 'views'));
 receiver.app.set('view engine', 'ejs');
 
-(async () => {
-  await app.start(ENV.PORT);
-  console.log('⚡ FDTParkingBot running locally!');
-  console.log(`Server running at http://localhost:${ENV.PORT}`);
-})();
+export default receiver.app;
+
